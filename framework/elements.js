02d0ea399903acc0d0801f3b793165f330fc618d
kyohei-yamashita <kyohei37leon@gmail.com>
2022-02-10 17:12:45 +0900

    使い回しできるように修正

--- framework/elements.js
+++ framework/elements.js
@@ -1,13 +1,36 @@
-const div = (strings, ...args) => {
-  return strings.reduce(
-    (previouse, currentString, index) =>
-      previouse + currentString + (args[index] || ""),
-    ""
-  );
-};
+/**
+ * createElementの大元はこんな感じ。
+ * これをリファクタして使いまわせるようにした
+ */
 
-const firstName = "Kyohei";
-const lastName = "Yama";
+// const div = (strings, ...args) => {
+//     // ""で初期値を設定して、順に文字列に戻している
+//     // 引数の順番がどうであろうと、placeholderと交互に展開する
+//     // placeholderが連w続する場合にはstringsにブランクが追加される
+//     // よっていい感じに順番が保持されて出力される
+//   return strings.reduce(
+//     (previouse, currentString, index) =>
+//       previouse + currentString + (args[index] || ""),
+//     ""
+//   );
+// };
 
-const result = div`Hello ${firstName} ${lastName} san`;
-console.log(result);
+// const firstName = "Kyohei";
+// const lastName = "Yama";
+
+// const result = div`Hello aa ${firstName} ${"aa"} ああ ${lastName} san`;
+
+const createElement =
+  (tagName) =>
+  (strings, ...args) => ({
+    type: tagName,
+    template: strings.reduce(
+      (prev, current, index) => prev + current + (args[index] || ""),
+      ""
+    ),
+  });
+
+const p = createElement("p");
+
+const { template } = p`Hello ${"yama"} 3`;
+console.log(template);
